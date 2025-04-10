54f7353418d2bffd392f18ca4d1b04197519c0c0
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-04-10 10:46:16 -0600

    update styles

--- src/app/admin-certificates/admin-certificates.component.ts
+++ src/app/admin-certificates/admin-certificates.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CertificatesService } from '../services/certificates-service/certificates.service';
+import { Certificates } from '../models/certificates/certificates.model';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-certificates',
@@ -6,5 +9,76 @@ import { Component } from '@angular/core';
   styleUrl: './admin-certificates.component.css'
 })
 export class AdminCertificatesComponent {
+  itemCount: number = 0;
+  btnTxt: string = "Agregar";
+  goalText: string = "";
+  isEditing: boolean = false;
+editingId: string | null = null;
+
+  certificates: Certificates[] = [];
+  myCertificates: Certificates = new Certificates();
+
+  constructor(public certificatesService: CertificatesService) {
+    console.log(this.certificatesService);
+    this.certificatesService.getCertificates().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      this.certificates = data;
+      console.log(this.certificates);
+    });
+  }
+
+  AgregarJob() {
+  if (this.isEditing && this.editingId) {
+    if (confirm("¿estas seguro de actualizar el certificado?")) {
+      this.certificatesService.updateCertificates(this.editingId, this.myCertificates).then(() => {
+        console.log('Item updated successfully!');
+        this.resetForm();
+      });
+    }
+  } else {
+    if (confirm("¿estas seguro de agregar este nuevo certificado?")) {
+      this.certificatesService.createCertificates(this.myCertificates).then(() => {
+        console.log('Created new item successfully!');
+        this.resetForm();
+      });
+    }
+  }
+}
+
+
+resetForm() {
+  this.myCertificates = new Certificates();
+  this.btnTxt = "Agregar";
+  this.isEditing = false;
+  this.editingId = null;
+}
+
+
+deleteJob(id?: string) {
+  if (confirm("¿estas seguro de eliminar este certificado?")) {
+    this.certificatesService.deleteCertificates(id).then(() => {
+      console.log('Item deleted successfully!');
+    });
+    console.log(id);
+  }
+}
+
+updateJob(id?: string) {
+  const certToEdit = this.certificates.find(cert => cert.id === id);
+  if (certToEdit) {
+    this.myCertificates = { ...certToEdit }; 
+    this.isEditing = true;
+    this.editingId = id || null;
+    this.btnTxt = "Actualizar";
+  }
+}
+
+
+
 
 }
--- src/app/admin-interests/admin-interests.component.ts
+++ src/app/admin-interests/admin-interests.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { InterestsService } from '../services/interests-service/interests.service';
+import { Interests } from '../models/interests/interests.model';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-admin-interests',
@@ -6,5 +10,74 @@ import { Component } from '@angular/core';
   styleUrl: './admin-interests.component.css'
 })
 export class AdminInterestsComponent {
+	itemCount: number = 0;
+  btnTxt: string = "Agregar";
+  goalText: string = "";
+  isEditing: boolean = false;
+  editingId: string | null = null;
+  interests: Interests[] = [];
+  myInterests: Interests = new Interests();
+
+  constructor(public interestsService: InterestsService) {
+    console.log(this.interestsService);
+    this.interestsService.getInterests().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c =>
+          ({ id: c.payload.doc.id, ...c.payload.doc.data() })
+        )
+      )
+    ).subscribe(data => {
+      this.interests = data;
+      console.log(this.interests);
+    });
+  }
+
+
+AgregarJob() {
+      if (this.isEditing && this.editingId) {
+        if (confirm("¿estas seguro de actualizar el interests?")) {
+          this.interestsService.updateInterests(this.editingId, this.myInterests).then(() => {
+            console.log('Item updated successfully!');
+            this.resetForm();
+          });
+        }
+      } else {
+        if (confirm("¿estas seguro de agregar este nuevo interests?")) {
+          this.interestsService.createInterests(this.myInterests).then(() => {
+            console.log('Created new item successfully!');
+            this.resetForm();
+          });
+        }
+      }
+    }
+    
+    
+    resetForm() {
+      this.myInterests = new Interests();
+      this.btnTxt = "Agregar";
+      this.isEditing = false;
+      this.editingId = null;
+    }
+    
+    
+    deleteJob(id?: string) {
+      if (confirm("¿estas seguro de eliminar este interests?")) {
+        this.interestsService.deleteInterests(id).then(() => {
+          console.log('Item deleted successfully!');
+        });
+        console.log(id);
+      }
+    }
+    
+    updateJob(id?: string) {
+      const certToEdit = this.interests.find(cert => cert.id === id);
+      if (certToEdit) {
+        this.myInterests = { ...certToEdit }; 
+        this.isEditing = true;
+        this.editingId = id || null;
+        this.btnTxt = "Actualizar";
+      }
+    }
+
 
 }
